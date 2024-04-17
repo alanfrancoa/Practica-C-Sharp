598265d3058e6a62bbf544beede07fa05f3825ec
alanfrancoa <alvarezalanfranco@gmail.com>
2024-04-17 09:50:43 -0300

    Se realiza calculadora de nota media Global, proyecto final de Escritura del primer código con C# parte 1 por Learn Microsoft.

---
+++ Calculadora-Puntaje/Calculadora-Puntaje/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Puntaje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Estamos desarrollando una calculadora de nota media global de los alumnos que ayudará a calcular el promedio global de las calificaciones de los alumnos. Los parámetros de la aplicación son:
+                -Se nos proporciona el nombre y la información de la clase del alumno.
+                -Cada clase tiene un nombre, la calificación del alumno y el número de horas en créditos de esa clase.
+                -La aplicación debe realizar operaciones matemáticas básicas para calcular la nota media global del alumno determinado.
+                -La aplicación debe generar o mostrar el nombre del alumno, la información de la clase y la nota media global.
+            Para calcular la nota media global:
+                -Multiplique la nota de un curso por el número de horas en créditos de ese curso.
+                -Hágalo para cada curso y reúna los resultados.
+                -Divida la suma resultante por el número total de horas en créditos.*/
+
+            string studentName = "Sophia Johnson";
+            string course1Name = "English 101";
+            string course2Name = "Algebra 101";
+            string course3Name = "Biology 101";
+            string course4Name = "Computer Science I";
+            string course5Name = "Psychology 101";
+
+            int course1Credit = 3;
+            int course2Credit = 3;
+            int course3Credit = 4;
+            int course4Credit = 4;
+            int course5Credit = 3;
+
+            int gradeA = 4;
+            int gradeB = 3;
+
+            int course1Grade = gradeA;
+            int course2Grade = gradeB;
+            int course3Grade = gradeB;
+            int course4Grade = gradeB;
+            int course5Grade = gradeA;
+
+            int totalCreditHours = 0;
+            totalCreditHours += course1Credit;
+            totalCreditHours += course2Credit;
+            totalCreditHours += course3Credit;
+            totalCreditHours += course4Credit;
+            totalCreditHours += course5Credit;
+
+            int totalGradePoints = 0;
+            totalGradePoints += course1Credit * course1Grade;
+            totalGradePoints += course2Credit * course2Grade;
+            totalGradePoints += course3Credit * course3Grade;
+            totalGradePoints += course4Credit * course4Grade;
+            totalGradePoints += course5Credit * course5Grade;
+
+            decimal gradePointAverage = (decimal)totalGradePoints / totalCreditHours;
+
+            //Obtener parte entera y primeros dos decimales. 
+            int leadingDigit = (int)gradePointAverage;
+            int firstDigit = (int)(gradePointAverage * 10) % 10;
+            int secondDigit = (int)(gradePointAverage * 100) % 10;
+
+            Console.WriteLine($"Student: {studentName}\n");
+            Console.WriteLine("Course\t\t\t\tGrade\tCredit Hours");
+
+            Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t\t{course1Credit}");
+            Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
+            Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
+            Console.WriteLine($"{course4Name}\t\t{course4Grade}\t\t{course4Credit}");
+            Console.WriteLine($"{course5Name}\t\t\t{course5Grade}\t\t{course5Credit}");
+
+            Console.WriteLine($"\nFinal GPA:\t\t\t {leadingDigit}.{firstDigit}{secondDigit}");
+            Console.ReadKey();
+        }
+    }
+}
